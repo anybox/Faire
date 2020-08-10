90b69da2cd787baa671c34f66959e108cd9faf05
Valentin DION <vdion@anybox.fr>
2020-08-10 22:31:34 +0200

    walkthrough

--- faire/tests/eventstore_test.py
+++ faire/tests/eventstore_test.py
@@ -12,13 +12,13 @@ class DummyEvent(Event):
 def test_EventStore():
     store = EventStore("test_store")
 
-    store.add_event(DummyEvent(stream_id='stream1'))
-    store.add_event(DummyEvent(stream_id='stream1'))
-    store.add_event(DummyEvent(stream_id='stream2'))
-    store.add_event(DummyEvent(stream_id='stream1'))
-    store.add_event(DummyEvent(stream_id='stream3'))
-    store.add_event(DummyEvent(stream_id='stream2'))
+    store.add_event(DummyEvent(stream_id="stream1"))
+    store.add_event(DummyEvent(stream_id="stream1"))
+    store.add_event(DummyEvent(stream_id="stream2"))
+    store.add_event(DummyEvent(stream_id="stream1"))
+    store.add_event(DummyEvent(stream_id="stream3"))
+    store.add_event(DummyEvent(stream_id="stream2"))
 
-    assert len(store.get_stream('stream1'))  == 3
-    assert len(store.get_stream('stream2'))  == 2
-    assert len(store.get_stream('stream3'))  == 1
+    assert len(store.get_stream("stream1")) == 3
+    assert len(store.get_stream("stream2")) == 2
+    assert len(store.get_stream("stream3")) == 1
